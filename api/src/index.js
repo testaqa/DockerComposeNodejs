aca2960e87e9320af4d0f35f6017ff54d283feaa
tester <test@test.com>
2021-05-21 14:33:16 +0000

    Mongo database for api

--- api/src/index.js
+++ api/src/index.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const { connectDb } = require("./helpers/db");
+const { host, port, db } = require("./configuration")
+
 const app = express();
-const port = process.env.PORT;
-const host = process.env.HOST;
+
+const startServer = () => {
+  app.listen(port, () => {
+    console.log("Started api service on port: " + port);
+    console.log("On host " + host)
+    console.log("Our database " + db)
+  });
+};
 
 app.get("/test", (req, res) => {
   res.send("Our api server is working correctly");
 });
 
-app.listen(port, () => {
-  console.log("Started api service on port: " + port);
-  console.log("On host " + host)
-});
+connectDb()
+  .on("error", console.log)
+  .on("disconnected", connectDb)
+  .once("open", startServer);
