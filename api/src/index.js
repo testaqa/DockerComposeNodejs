1d3ad00e582f063fed879614e1db0d55e4aa9903
tester <test@test.com>
2021-05-21 06:30:14 +0000

    Restart until stopped, env variables to docker-compose.yml and index.js

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const app =express();
+const app = express();
+const port = process.env.PORT;
+const host = process.env.HOST;
 
 app.get("/test", (req, res) => {
   res.send("Our api server is working correctly");
 });
 
-app.listen(3000, () => {
-  console.log("Started api service");
+app.listen(port, () => {
+  console.log("Started api service on port: " + port);
+  console.log("On host " + host)
 });
