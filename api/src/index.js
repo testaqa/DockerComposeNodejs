26dc4b5e51df3c12e0e840d9f41b24abe347bea3
tester <test@test.com>
2021-05-24 05:21:01 +0000

    Save entity to db, volume for db, docker-compose for dev env, nodemon for restart on changes

--- api/src/index.js
+++ api/src/index.js
@@ -1,14 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const { connectDb } = require("./helpers/db");
 const { host, port, db } = require("./configuration")
 
 const app = express();
 
+const postSchema = new mongoose.Schema({
+  name: String
+});
+const Post = mongoose.model("Post", postSchema);
+
+
 const startServer = () => {
   app.listen(port, () => {
     console.log("Started api service on port: " + port);
-    console.log("On host " + host)
-    console.log("Our database " + db)
+    console.log("On host " + host);
+    console.log("Our database " + db);
+
+    const silence = new Post({ name: "Silence" });
+    silence.save(function(err, savedSilence) {
+      if (err) return console.error(err);
+      console.log("savedSilence", savedSilence);
+    });
   });
 };
 
