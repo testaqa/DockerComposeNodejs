67ff6ac7d2dd39f46a0f7b05a9533c41e10ad3dc
tester <test@test.com>
2021-05-24 05:42:43 +0000

    Auth project copied from api project

---
+++ auth/src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { connectDb } = require("./helpers/db");
+const { host, port, db } = require("./configuration")
+
+const app = express();
+
+const startServer = () => {
+  app.listen(port, () => {
+    console.log("Started auth service on port: " + port);
+    console.log("On host " + host);
+    console.log("Our database " + db);
+  });
+};
+
+app.get("/test", (req, res) => {
+  res.send("Our auth server is working correctly");
+});
+
+connectDb()
+  .on("error", console.log)
+  .on("disconnected", connectDb)
+  .once("open", startServer);
